6da16e5fe587d65d228896b3cf7757b1ac954d7c
TAHREEM ARSHAD <tahreema636@gmail.com>
2025-04-23 20:00:02 +0500

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,6 @@
+// Basic form submission handling
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+  });
+  
